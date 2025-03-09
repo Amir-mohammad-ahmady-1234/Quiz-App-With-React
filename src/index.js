76ce8ceb30a9633d40d1d92279d0dc56dec3b65d
amirMohammad <amyrmhmdahmdy179@gmail.com>
2025-03-09 12:34:13 -0700

    index.js updated

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import react from "react";
 import ReactDOM from "react-dom/client";
-import { StrictMode } from "react";
+import StrictMode from "react";
 import "./style.css";
 import App from "./components/App";
 
